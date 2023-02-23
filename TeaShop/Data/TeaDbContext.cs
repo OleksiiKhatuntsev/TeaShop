ae20fb371dfba29998ddb4f9f12beb9f14234236
Oleksii Khatuntsev <oleksii_khatuntsev@symbotic.com>
2023-02-23 22:31:34 +0100

    Add project files.

---
+++ TeaShop/Data/TeaDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeaShop.Models;
+
+namespace TeaShop.Data
+{
+    public class TeaDbContext : DbContext
+    {
+        public TeaDbContext(DbContextOptions<TeaDbContext> options) : base(options)
+        { }
+
+        public DbSet<Category> Category { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json")
+                                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+    }
+}
